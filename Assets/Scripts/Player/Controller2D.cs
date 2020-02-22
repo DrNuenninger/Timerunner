c72bf3d2fbe16da901f96115bb7ea05215a53bd3
Ceepert <niclastachilzik@web.de>
2020-02-22 17:36:55 +0100

    Started working on Raycasting based movement
    The currently used Movement System is ok, but has some flaws that cant be fixed in a nice manner without alot of extra effort, so I am starting to work on a Raycasting based Movement System that wont need ridged Bodies, based on a tutorial

---
+++ Assets/Scripts/Player/Controller2D.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour
+{
+    private BoxCollider2D collider;
+    private RayCastOrigins rayCastOrigins;
+    const float skinwidth = 0.015f;
+
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    private float horizontalRaySpacing;
+    private float verticalRaySpacing;
+
+
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    struct RayCastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+
+    void UpdateRayCastOrigins()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinwidth * -2);
+
+        rayCastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        rayCastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        rayCastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y); 
+        rayCastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinwidth * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateRayCastOrigins();
+        CalculateRaySpacing();
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Debug.DrawRay(rayCastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2,Color.red);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    private Controller2D controller;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
