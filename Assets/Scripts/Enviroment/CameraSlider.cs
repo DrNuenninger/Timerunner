4d90feb99cf51e3b9359a3831c844bdb3aefaac9
Ceepert <niclastachilzik@web.de>
2020-02-22 11:56:22 +0100

    [Added] Camera Slider
    Basic Camera Slider
    Very Stiff, needs smoothening
    can lock x or y axis at will
    has zoom option, doesnt do anything yet

---
+++ Assets/Scripts/Enviroment/CameraSlider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSlider : MonoBehaviour
+{
+    public Rigidbody2D player;
+    public bool lockX = false;
+    public bool lockY = false;
+    public int zoom = -10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (!lockY)
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y, zoom);
+        }
+        if (!lockX)
+        {
+            transform.position = new Vector3(player.position.x, transform.position.y, zoom);
+        }
+    }
+}
