205c743c7d2cc9a9ea210e501ded8628ce18bdf8
flarmit <RalfSchnatterer@googlemail.com>
2020-03-03 14:45:56 +0100

    basic work for Main Menu
    Added some classes and a scene

---
+++ Assets/Scripts/Game/Game_Save.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Save : MonoBehaviour
+{
+    public int currentLevel;
+    public ArrayList levelList = new ArrayList();
+    public string levelPath = "";
+
+    public void loadLevelList(string levelPath)
+    {
+        //loads the set of created levels into the ArrayList; gets called on Start of the Game
+    }
+}
---
+++ Assets/Scripts/Game/Load_Manager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load_Manager : MonoBehaviour //Loads and manages available Save-Games
+{
+    
+}
