f9fddcb9586c0508cf035c7b998ab93a793b0c60
flarmit <RalfSchnatterer@googlemail.com>
2020-03-09 13:28:49 +0100

    Added Escape To Main-Menu
    still don't know what all these animations meta files want here... hope they don't mess anything up

--- Assets/Scripts/Game/Load_Manager.cs
+++ Assets/Scripts/Game/Load_Manager.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Load_Manager : MonoBehaviour //Loads and manages available Save-Games
 {
+    public static Load_Manager load_Manager;
+    void Start()
+    {
+        if (load_Manager)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            load_Manager = this;
+        }
+    }
     public void loadDemo()
     {
         SceneManager.LoadScene("Demo_Level", LoadSceneMode.Single);
+    }
+    public void escape_Action()
+    {
+        SceneManager.LoadScene("Main_Menu", LoadSceneMode.Single);
     }
 }
