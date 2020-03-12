fb3a6c010150034ff6767216b43078e4b7ebb98e
Ceepert <niclastachilzik@web.de>
2020-03-12 17:12:54 +0100

    [Added] Kill Player Script

---
+++ Assets/Scripts/Player/KillPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+
+    void OnCollisionEnter2D(Collider2D col)
+    {
+
+    }
+}
