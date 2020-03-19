f97f71b9117595c87f35e9ba8d0c635685c57010
DrNuenninger <tillmann@nuenninghoff.de>
2020-03-20 00:19:53 +0100

    At finish prefab

---
+++ Assets/Scripts/Enviroment/FinishBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishBehavior : MonoBehaviour
+{
+    Load_Manager loadManagerController;
+
+    void Start()
+    {
+        loadManagerController = GameObject.Find("LoadManagerController").GetComponent<Load_Manager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            loadManagerController.load_LevelSelect();
+        }
+    }
+}
