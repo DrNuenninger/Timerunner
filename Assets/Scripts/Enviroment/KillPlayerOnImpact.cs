7b4d10aba8b6b27535ba0a99c23007b1b8373d36
DrNuenninger <tillmann@nuenninghoff.de>
2020-03-20 17:15:30 +0100

    New Skript to attach to objects which should kill the player

---
+++ Assets/Scripts/Enviroment/KillPlayerOnImpact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayerOnImpact : MonoBehaviour
+{
+    Controller2D controller2DPlayer;
+
+    void Start()
+    {
+        controller2DPlayer = GameObject.Find("Player").GetComponent<Controller2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            controller2DPlayer.isDead = true;
+        }
+    }
+}
