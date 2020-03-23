8528a081b2bd25ca057bbd038729d9f9d11b0aaa
DrNuenninger <tillmann@nuenninghoff.de>
2020-03-24 00:12:46 +0100

    [Add] Missle prefab with imported explosion

---
+++ Assets/Scripts/Enemies/Missle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missle : MonoBehaviour
+{
+    public GameObject explosionEffect;
+    public float speed = 5f;
+    private Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = transform.up * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(Instantiate(explosionEffect, transform.position, transform.rotation), 1); 
+        Collider2D[] damageableInRadius = Physics2D.OverlapCircleAll(transform.position, 1.5f, LayerMask.GetMask("Player"));
+        if (damageableInRadius.Length == 1)
+        {
+            damageableInRadius[0].gameObject.GetComponent<Controller2D>().isDead = true;
+        }
+        Destroy(gameObject);
+    }
+}
