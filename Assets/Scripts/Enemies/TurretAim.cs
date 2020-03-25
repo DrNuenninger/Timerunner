e68c3a52e2a019d211106b5f11c528a07e121cc3
DrNuenninger <tillmann@nuenninghoff.de>
2020-03-25 15:26:15 +0100

    Turrets aim correctly at the player

--- Assets/Scripts/Enemies/TurretAim.cs
+++ Assets/Scripts/Enemies/TurretAim.cs
@@ -8,58 +8,51 @@ public class TurretAim : MonoBehaviour
     public float maxLeftAngle = 180f;
     public float minRightAngle = 0f;
     public float maxRightAngle = 180f;
-    private Transform rotationAnchorpoint;
-    private Transform barrelPosition0;
+    public float aimSpeed = 0.8f;
+
+    public Transform rotationAnchorpoint;
+    public Transform barrelPosition0;
+    public Transform barrel_Tip;
+    public Transform barrel;
+
+    public bool isActive = false;
+    public float angleToPlayer = 0.0f;
+
     private Transform player;
+    private SpriteRenderer barrelSprite;
     private Vector3 barrelDirection;
-    private Vector3 barrelToAnchorpointDirection;
-    private Quaternion q;
     private float angle = 0.0f;
-    private float angle2 = 0.0f;
-    private Transform Barrel_Tip;
+
     void Start()
     {
-        rotationAnchorpoint = GameObject.Find("Rotation_anchorpoint").transform;
         player = GameObject.Find("Player").transform;
-        barrelPosition0 = GameObject.Find("Barrel_position0").transform;
-        Barrel_Tip = GameObject.Find("Barrel_Tip").transform;
 
         barrelDirection = rotationAnchorpoint.position - barrelPosition0.position;
-        barrelToAnchorpointDirection = (transform.position - rotationAnchorpoint.position);
+        barrelSprite= barrel.GetComponent<SpriteRenderer>();
+
     }
 
     void Update()
     {
         angle = Vector2.Angle(barrelDirection, rotationAnchorpoint.position - player.position);
-        float direction = Vector3.Cross(barrelDirection, rotationAnchorpoint.position - player.position).normalized.z * -1;
+        float direction = Vector3.Cross(barrelDirection, rotationAnchorpoint.position - player.position).normalized.z;
         angle = angle * direction;
 
-        if (direction == 1)
+        angleToPlayer = Vector2.Angle(rotationAnchorpoint.position - barrel_Tip.position, rotationAnchorpoint.position - player.position);
+
+        barrelSprite.flipX = rotationAnchorpoint.localRotation.z > 0;
+
+        if (direction == -1)
         {
-            angle = Mathf.Clamp(angle, minRightAngle, maxRightAngle);
+            angle = Mathf.Clamp(angle, maxRightAngle * -1,minRightAngle * -1);     
         }
         else
         {
-            angle = Mathf.Clamp(angle, maxLeftAngle * -1, minLeftAngle *-1);
+            angle = Mathf.Clamp(angle, minLeftAngle, maxLeftAngle); 
         }
 
-        q = Quaternion.AngleAxis(angle, Vector3.back);
-      
-        transform.position = rotationAnchorpoint.position + q * barrelToAnchorpointDirection;
-        transform.rotation = q;
-    }
-
-    void OnGUI()
-    {
-        var difference = q.eulerAngles - transform.rotation.eulerAngles;
-        GUI.Label(new Rect(10, 10, 100, 20), "Angle: " + angle);
-
-        GUI.Label(new Rect(10, 30, 200, 20), "Quaternion: " + q.eulerAngles);
-        GUI.Label(new Rect(10, 50, 200, 20), "Transform: " + transform.rotation.eulerAngles);
-        GUI.Label(new Rect(10, 70, 200, 20), "Difference: " + difference);
-        GUI.Label(new Rect(10, 90, 200, 20), "Quaternion: " + q);
-        GUI.Label(new Rect(10, 110, 200, 20), "CurrentAngle: " + angle2);
-
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        rotationAnchorpoint.localRotation = Quaternion.RotateTowards(rotationAnchorpoint.localRotation, rotation, aimSpeed);       
     }
 }
 
--- Assets/Scripts/Enemies/TurretBehaviour.cs
+++ Assets/Scripts/Enemies/TurretBehaviour.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class TurretBehaviour : MonoBehaviour
 {
+    public float delayBetweenShots = 4f;
+    public float activationRange = 5f;
+    public float shootingMargin = 10f;
     public GameObject missle;
-    private Transform shootingPosition;
-    public float DelayBetweenShots = 4f;
-    private float DelayToPass = 0f; 
+    public Transform shootingPosition;
+    private TurretAim turretAim;
+    private Transform player;
+    private float DelayToPass = 0.1f; 
 
     void Start()
     {
-        shootingPosition = GameObject.Find("Barrel_Tip").transform;
+        player = GameObject.Find("Player").transform;
+        turretAim = gameObject.GetComponent<TurretAim>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (DelayToPass <= 0)
+        var distance = (player.position - transform.position).magnitude;
+
+        if (distance <= activationRange)
         {
-            Instantiate(missle, shootingPosition.position, shootingPosition.rotation);
-            DelayToPass = DelayBetweenShots;
+            turretAim.isActive = true;
+        }
+        else
+        {
+            turretAim.isActive = false;
+        }
+        if (DelayToPass <= 0)
+        {   
+            if (turretAim.angleToPlayer <= shootingMargin)
+            {
+                Instantiate(missle, shootingPosition.position, shootingPosition.rotation);
+                DelayToPass = delayBetweenShots;
+            }
         }
         else
         {
