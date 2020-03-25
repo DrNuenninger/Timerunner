370a231a01b6b7e92e3ea027286defcc33ec0ed4
DrNuenninger <tillmann@nuenninghoff.de>
2020-03-25 15:25:24 +0100

    Missle explosion and particles

--- Assets/Scripts/Enemies/Missle.cs
+++ Assets/Scripts/Enemies/Missle.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Missle : MonoBehaviour
 {
     public GameObject explosionEffect;
+    private new Rigidbody2D rigidbody;
     public float speed = 5f;
-    private Rigidbody2D rigidbody;
 
     void Start()
     {
@@ -16,7 +16,7 @@ public class Missle : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(Instantiate(explosionEffect, transform.position, transform.rotation), 1); 
+        Destroy(Instantiate(explosionEffect, transform.position, transform.rotation), 3); 
         Collider2D[] damageableInRadius = Physics2D.OverlapCircleAll(transform.position, 1.5f, LayerMask.GetMask("Player"));
         if (damageableInRadius.Length == 1)
         {
