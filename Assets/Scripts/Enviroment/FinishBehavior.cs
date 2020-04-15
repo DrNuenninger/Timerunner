05a0c838b6fe34134b4284b34ee021d3ddea3008
DrNuenninger <tillmann@nuenninghoff.de>
2020-04-15 14:41:44 +0200

    Save highscore when exiting level

--- Assets/Scripts/Enviroment/FinishBehavior.cs
+++ Assets/Scripts/Enviroment/FinishBehavior.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class FinishBehavior : MonoBehaviour
 {
     Load_Manager loadManagerController;
+    Level_Information level_Information;
+    StartGame startGameScript;
+    HighscoreTable highscoreTable;
 
     void Start()
     {
         loadManagerController = GameObject.Find("LoadManagerController").GetComponent<Load_Manager>();
+        level_Information = GameObject.Find("Level_Information").GetComponent<Level_Information>();
+        startGameScript = GameObject.Find("Main Camera").GetComponent<StartGame>();
+        highscoreTable = new HighscoreTable();
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Player")
         {
+            highscoreTable.AddHighscoreEntry((int) startGameScript.timeGone,level_Information.sceneName);
             loadManagerController.loadNextLevel(); 
         }
     }
