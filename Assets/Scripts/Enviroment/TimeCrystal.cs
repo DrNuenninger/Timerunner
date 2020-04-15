020d1c53217d48fea11511de1eabab3cf1eac5d4
DrNuenninger <tillmann@nuenninghoff.de>
2020-04-15 14:38:09 +0200

    Add collectible Time crystal

---
+++ Assets/Scripts/Enviroment/TimeCrystal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeCrystal : MonoBehaviour
+{
+    public SwitchTime switchTime;
+    public HintLevel hintLevel;
+
+    void Start()
+    {
+        switchTime.timeSwitchPossible = false;
+        hintLevel.hintLevelPossible = false;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Destroy(gameObject);
+            switchTime.timeSwitchPossible = true;
+            hintLevel.hintLevelPossible = true;
+        }
+    }
+}
+
