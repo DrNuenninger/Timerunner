40ef746bac558a24161369058edda6b55b4fe982
DrNuenninger <tillmann@nuenninghoff.de>
2020-04-15 14:36:44 +0200

    Create prefab to display certain text over player character

---
+++ Assets/Scripts/Enviroment/TextOverPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextOverPlayer : MonoBehaviour
+{
+    public GameObject textToDisplay;
+    public float displayDuration = 3;
+    private bool currentlyDisplayed = false;
+    
+    void Update()
+    {
+        if (currentlyDisplayed)
+        {
+            DisplayTimer();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            DisplayText();
+        }
+
+    }
+
+    private void DisplayTimer()
+    {
+        if (displayDuration <= 0)
+        {
+            StopDisplayText();
+        }
+
+        displayDuration -= Time.deltaTime;
+    }
+
+    private void DisplayText()
+    {
+        currentlyDisplayed = true;
+        textToDisplay.SetActive(true);
+    }
+
+    private void StopDisplayText()
+    {
+        currentlyDisplayed = false;
+        textToDisplay.SetActive(false);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/TutorialPopup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPopup : MonoBehaviour
+{
+    public Transform player;
+
+    void Update()
+    {
+        gameObject.transform.position = player.position;
+        gameObject.transform.position += new Vector3(0,2,0);  
+    }
+}
